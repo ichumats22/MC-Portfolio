ad2859a0b7faff390ae54f43606b375aaabe3bb5
ichumats <ivanna.chumatschko22@gmail.com>
2020-04-11 22:18:06 -0700

    inital commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  $('#mySidenav').show()
+  $('#sidenav-btn').hide()
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  $('#mySidenav').hide()
+  $('#sidenav-btn').show()
+}
